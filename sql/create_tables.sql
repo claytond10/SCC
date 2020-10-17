ba56abc273147054d3dee69144dcbe78af6e6460
Clayton Dulaney <claytond10@gmail.com>
2020-10-17 18:56:04 -0500

    added 3 tables to table creation script

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -31,10 +31,43 @@ CREATE TABLE `scc`.`event` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC));
   
+CREATE TABLE `scc`.`image` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `image_html` VARCHAR(50) NOT NULL,
+  `image_name` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `image_name_UNIQUE` (`image_name` ASC));
+
+CREATE TABLE `scc`.`location` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `location_name` VARCHAR(50) NOT NULL,
+  `loc_cd` CHAR(2) NOT NULL,
+  `image_id` INT,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `location_name_UNIQUE` (`location_name` ASC));
   
-image (id, image_html, image_name)
-location (id, location_name, loc_cd, image_id)
-location_type (loc_cd, loc_cd_full)
-logistics (id, log_name, loc_id)
-schedule (id, s_date s_time, activity_id)
-scripture
+CREATE TABLE `scc`.`location_type` (
+  `loc_cd` CHAR(2) NOT NULL,
+  `loc_cd_full` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`loc_cd`),
+  UNIQUE INDEX `loc_cd_UNIQUE` (`loc_cd` ASC),
+  UNIQUE INDEX `loc_cd_full_UNIQUE` (`loc_cd_full` ASC));  
+
+CREATE TABLE `scc`.`logistics` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `log_name` VARCHAR(50) NOT NULL,
+  `loc_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `log_name_UNIQUE` (`log_name` ASC));
+
+CREATE TABLE `scc`.`schedule` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `s_date` VARCHAR(10) NOT NULL,
+  `s_time` VARCHAR(10) NOT NULL,
+  `activity_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+  
