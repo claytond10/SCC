6a40631fbee6faecb0aae0b7919bd4b6a559c7a6
Clayton Dulaney <claytond10@gmail.com>
2020-11-05 18:54:55 -0600

    Fix sp_GetLogisticsById.sql

--- sql/sp_GetLogisticsById.sql
+++ sql/sp_GetLogisticsById.sql
@@ -9,6 +9,6 @@ BEGIN
 	SELECT log.id log_id, log.log_name log_name, img.id image_id, loc.id location_id,
 		   img.image_name, img.image_html image_html, loc.location_name location_name
 	FROM logistics log LEFT JOIN location loc ON (log.loc_id = loc.id)
-					   LEFT JOIN image img ON (loc.image_id = img.id)
+					   LEFT JOIN image img ON (log.image_id = img.id)
 	WHERE log.id = iId;
 END
